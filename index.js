d879a4fa03aab1f1a6569c7e26d4b4ea6d94b458
BarhamBapirAhmad <barhamabpirahmed@gmail.com>
2024-02-29 05:51:11 +0300

    Done 0.0.1

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+const  test = [
+    {name : "susan" , height : 40 , message : `${name} is ${height} inches tall`},
+    {name : "Joe" , height : 74 , message : `${name} is ${height} inches tall`}
+ ]
